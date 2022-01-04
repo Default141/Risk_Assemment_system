9c3fa13458c91bd6fdf5de104be1b8be6a2f344d
Default141 <phoenixbird14@gmail.com>
2022-01-04 16:27:05 +0700

    complete ingestion

--- ingestion.py
+++ ingestion.py
@@ -19,8 +19,16 @@ output_folder_path = config['output_folder_path']
 #############Function for data ingestion
 def merge_multiple_dataframe():
     #check for datasets, compile them together, and write to an output file
-
-
+    filenlist = os.listdir(os.getcwd()+"/"+input_folder_path)
+    data_df = pd.DataFrame()
+    for file in filenlist:
+        # print("./" + input_folder_path + "/" + file)
+        currentdf = pd.read_csv("./" + input_folder_path + "/" + file)
+        data_df = data_df.append(currentdf)
+    data_df.to_csv("finaldata.csv")
+    f = open("./" + output_folder_path + "/" + "ingestedfiles.txt", "a")
+    f.write(str(filenlist))
+    f.close()
 
 if __name__ == '__main__':
     merge_multiple_dataframe()
